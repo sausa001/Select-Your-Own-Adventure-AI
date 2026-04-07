3e845da8a7fdb2278773fd3a69e1784d44dc0ef1
sausa001 <saketsaurabh.2@gmail.com>
2026-04-08 00:48:32 +0200

    Service URL is added in frontend from backend

--- frontend/src/util.js
+++ frontend/src/util.js
@@ -1,4 +1,6 @@
-export const API_BASE_URL = "http://127.0.0.1:8000";       
+//export const API_BASE_URL = "http://127.0.0.1:8000";     
+
+export const API_BASR_URL = "https://489467e6-cf6b-4257-9f43-274b49fe9e08-dev.e1-us-east-azure.choreoapis.dev/select-your-own-adventure/backend/v1"
 
 // export const API_BASE_URL = "https://select-your-own-adventure-ai.onrender.com";
 
