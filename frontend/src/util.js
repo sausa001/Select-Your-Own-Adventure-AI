85034c17eb5e45e9fa5ad78b16c01a5fd62f6a98
sausa001 <saketsaurabh.2@gmail.com>
2026-04-08 01:01:11 +0200

    Service URL/APIGatewayPath is added in frontend from backend

--- frontend/src/util.js
+++ frontend/src/util.js
@@ -1,6 +1,8 @@
+export const API_BASE_URL = "/choreo-apis/select-your-own-adventure/backend/v1/api";
+
 //export const API_BASE_URL = "http://127.0.0.1:8000";     
 
-export const API_BASR_URL = "https://489467e6-cf6b-4257-9f43-274b49fe9e08-dev.e1-us-east-azure.choreoapis.dev/select-your-own-adventure/backend/v1"
+// export const API_BASR_URL = "https://489467e6-cf6b-4257-9f43-274b49fe9e08-dev.e1-us-east-azure.choreoapis.dev/select-your-own-adventure/backend/v1"
 
 // export const API_BASE_URL = "https://select-your-own-adventure-ai.onrender.com";
 
