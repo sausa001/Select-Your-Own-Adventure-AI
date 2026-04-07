b1dc845fb83dc02a58b3091f91ac8deb30b26d8c
sausa001 <saketsaurabh.2@gmail.com>
2026-04-08 00:32:52 +0200

    serviceapi key  proxy added

--- backend/core/story_generator.py
+++ backend/core/story_generator.py
@@ -16,16 +16,18 @@ class StoryGenerator:
 
     @classmethod
     def _get_llm(cls):
-         openai_api_key = os.getenv("CHOREO_OPENAI_CONNECTION_OPENAI_API_KEY")
+        # openai_api_key = os.getenv("CHOREO_OPENAI_CONNECTION_OPENAI_API_KEY")
 
-         serviceurl = os.getenv("CHOREO_OPENAI_CONNECTION_SERVICEURL")
+       #  serviceurl = os.getenv("CHOREO_OPENAI_CONNECTION_SERVICEURL")
 
-        if openai_api_key and serviceurl:
-            return ChatOpenAI(model="gpt-4o-mini", api_key=openai_api_key, base_url=serviceurl)
+      #  if openai_api_key and serviceurl:
+           # return ChatOpenAI(model="gpt-4o-mini", api_key=openai_api_key, base_url=serviceurl)
 
 
 
-        return ChatOpenAI(model="gpt-4o-mini")
+        return ChatOpenAI(
+            model="gpt-4o-mini",
+            api_key=os.getenv("CHOREO_OPENAI_CONNECTION_OPENAI_API_KEY"),
 
 
     @classmethod
